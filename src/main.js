b001dbbc92cdaa3501705a199c98d17eefe22585
a365541113 <782343980@qq.com>
2019-07-09 16:13:14 +0800

    开路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+// 引入样式------这段没看懂
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: []
+})
+export default router
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
